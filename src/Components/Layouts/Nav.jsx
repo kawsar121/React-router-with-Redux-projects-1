5c90c999086b3213800d261708a6c227b668666f
kawsar121 <tohidulisamkawsarbhuiyan@gmail.com>
2025-12-14 18:55:16 +0600

    href

--- src/Components/Layouts/Nav.jsx
+++ src/Components/Layouts/Nav.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="bg-red-500 p-3">
-      <div className="flex justify-evenly ">
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-3">
+      <div className="flex justify-evenly text-lg font-semibold">
         <Link>Home</Link>
         <Link to="/todoapps">Todo Apps</Link>
         <Link>About</Link>
--- src/Pages/TodoApps/TodoApps.jsx
+++ src/Pages/TodoApps/TodoApps.jsx
@@ -2,7 +2,7 @@
 const TodoApps = () => {
     return (
         <div>
-            <h1>thi i todo</h1>
+            <h1 className="text-center mt-5 text-2xl font-bold divide-y text-red-500">Todo Apps ok.....</h1>
         </div>
     );
 };
