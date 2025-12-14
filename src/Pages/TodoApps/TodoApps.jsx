4f89504fa700e18a92ae55a574b09b913392c103
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2025-12-14 19:23:08 +0600

    all ok

--- src/Pages/TodoApps/TodoApps.jsx
+++ src/Pages/TodoApps/TodoApps.jsx
@@ -3,6 +3,7 @@ const TodoApps = () => {
     return (
         <div>
             <h1 className="text-center mt-5 text-2xl font-bold divide-y text-red-500">Todo Apps ok.....</h1>
+            <p>all ok test</p>
         </div>
     );
 };
